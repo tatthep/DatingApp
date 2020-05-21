c1f3fe6a4f7d83d05751998126a859f8eddfa1d9
unknown <tatthep@vk1.biz>
2020-05-21 14:24:25 +0700

    Intial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace DatingApp.API.Data {
+
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions<DataContext> options):base(options) {
+            //
+        }
+
+        public DbSet<Value> Values {get;set;}
+    }
+}
